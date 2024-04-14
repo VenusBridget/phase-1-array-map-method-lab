441cf6a3a810a222c2ec00a09634954c069c3525
VenusBridget <venusbri27@gmail.com>
2024-04-14 09:47:51 +0300

    initial commit

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    // Split the title into words
+    const words = title.split(' ');
+
+    // Capitalize the first letter of each word
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    // Join the capitalized words back into a title
+    return capitalizedWords.join(' ');
+  });
 }
+
+// Test the function
+console.log(titleCased());
